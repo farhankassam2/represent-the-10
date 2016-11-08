d19eeaa8c0cc4e33fe2ae6f8a45749911ab52ab2
Seung Won Lee <leesw@dhcp-128-189-79-126.ubcsecure.wireless.ubc.ca>
2016-11-08 12:42:07 -0800

    Submission with AJAX and no redirect

--- app/js/petition.js
+++ app/js/petition.js
@@ -8,49 +8,39 @@ $(document).ready(function() {
     var postalcode = $("input.darkgrey.form-control.postal").val();
 
     var valid=true;
-    if (stringValidate(fname)) {
-      return;
-    } else {
+
+    // Alerts when input is invalid
+    if (!stringValidate(fname)) {
       valid=false;
       alert("The first name field is invalid! It must only have characters and spaces.");
     }
 
-    if (stringValidate(lname)) {
-      return;
-    } else {
+    if (!stringValidate(lname)) {
       valid=false;
       alert("The last name field is invalid! It must only have characters and spaces.");
     }
 
-    if (stringAndNumberValidate(address)) {
-      return;
-    } else {
+    if (!stringAndNumberValidate(address)) {
       valid=false;
       alert("The address field is invalid! It must only have characters, numbers and spaces.");
     }
 
-    if (stringValidate(city)) {
-      return;
-    } else {
+    if (!stringValidate(city)) {
       valid=false;
       alert("The city field is invalid! It must only have characters and spaces.");
     }
 
-    if (stringValidate(province)) {
-      return;
-    } else {
+    if (!stringValidate(province)) {
       valid=false;
       alert("The province field is invalid! It must only have characters and spaces.");
     }
 
-    if (postalValidate(postalcode)) {
-      return;
-    } else {
+    if (!postalValidate(postalcode)) {
       valid=false;
       alert("The postal code field is invalid! It must only have characters, numbers and spaces.");
     }
 
-    //Validation Functions
+    //Validation Functions - Returns true when input is valid
     function stringValidate(str) {
       var regex = /[^a-z,\s]/gi;
       return !regex.test(str);
@@ -63,13 +53,38 @@ $(document).ready(function() {
     function postalValidate(str) {
       var regex = /[^a-z,\d]/gi;
       str = str.replace(" ", "")
-      return regex.test(str) && (str.length == 6);
+      return !(regex.test(str) && (str.length == 6));
     }
-    /*
+
+    //submitForm with ajax when valid
     if (valid) {
-      submitForm(fname, lname, address, city, province, postalcode);
+      $.ajax({
+                url: "https://docs.google.com/forms/d/e/1FAIpQLScMawBqg4oxvJVarWwRPjDpqtSu5MUdNq_dBgphyruz5e8z0g/formResponse",
+                data: {"entry.843751608" : fname, "entry.786666914" : lname, "entry.113893232": address, "entry.475910473": city, "entry.1430871484": province, "entry.2000257846": postalcode},
+                type: "POST",
+                dataType: "xml",
+                statusCode: {
+                    0: function (){
+                      //Clearing fields after submission
+                      $("input.darkgrey.form-control.fnm").val("");
+                      $("input.darkgrey.form-control.lnm").val("");
+                      $("input.darkgrey.form-control.address").val("");
+                      $("input.darkgrey.form-control.city").val("");
+                      $("input.darkgrey.form-control.province").val("");
+                      $("input.darkgrey.form-control.postal").val("");
+                    },
+                    200: function (){
+                      //Clearing fields after submission
+                      $("input.darkgrey.form-control.fnm").val("");
+                      $("input.darkgrey.form-control.lnm").val("");
+                      $("input.darkgrey.form-control.address").val("");
+                      $("input.darkgrey.form-control.city").val("");
+                      $("input.darkgrey.form-control.province").val("");
+                      $("input.darkgrey.form-control.postal").val("");
+                    }
+                }
+            });
     }
-    */
   })
 
 })
