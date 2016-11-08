c76cdf3b939113aabcab581eec4755b6d8531935
Seung Won Lee <leesw@dhcp-128-189-79-126.ubcsecure.wireless.ubc.ca>
2016-11-08 13:27:00 -0800

    Minor Optimizations

--- app/js/petition.js
+++ app/js/petition.js
@@ -7,36 +7,26 @@ $(document).ready(function() {
     var province = $("input.darkgrey.form-control.province").val();
     var postalcode = $("input.darkgrey.form-control.postal").val();
 
-    var valid=true;
+    var VALID = true;
 
     // Alerts when input is invalid
     if (!stringValidate(fname)) {
-      valid=false;
+      VALID=false;
       alert("The first name field is invalid! It must only have characters and spaces.");
-    }
-
-    if (!stringValidate(lname)) {
-      valid=false;
+    } else if (!stringValidate(lname)) {
+      VALID=false;
       alert("The last name field is invalid! It must only have characters and spaces.");
-    }
-
-    if (!stringAndNumberValidate(address)) {
-      valid=false;
+    } else if (!stringAndNumberValidate(address)) {
+      VALID=false;
       alert("The address field is invalid! It must only have characters, numbers and spaces.");
-    }
-
-    if (!stringValidate(city)) {
-      valid=false;
+    } else if (!stringValidate(city)) {
+      VALID=false;
       alert("The city field is invalid! It must only have characters and spaces.");
-    }
-
-    if (!stringValidate(province)) {
-      valid=false;
+    } else if (!stringValidate(province)) {
+      VALID=false;
       alert("The province field is invalid! It must only have characters and spaces.");
-    }
-
-    if (!postalValidate(postalcode)) {
-      valid=false;
+    } else if (!postalValidate(postalcode)) {
+      VALID=false;
       alert("The postal code field is invalid! It must only have characters, numbers and spaces.");
     }
 
@@ -45,7 +35,6 @@ $(document).ready(function() {
       var regex = /[^a-z,\s]/gi;
       return !regex.test(str);
     }
-
     function stringAndNumberValidate(str) {
       var regex = /[^a-z,\s,\d]/gi;
       return !regex.test(str);
@@ -56,11 +45,16 @@ $(document).ready(function() {
       return !(regex.test(str) && (str.length == 6));
     }
 
-    //submitForm with ajax when valid
-    if (valid) {
+    //Submit form with ajax if valid
+    if (VALID) {
       $.ajax({
                 url: "https://docs.google.com/forms/d/e/1FAIpQLScMawBqg4oxvJVarWwRPjDpqtSu5MUdNq_dBgphyruz5e8z0g/formResponse",
-                data: {"entry.843751608" : fname, "entry.786666914" : lname, "entry.113893232": address, "entry.475910473": city, "entry.1430871484": province, "entry.2000257846": postalcode},
+                data: {"entry.843751608" : fname,
+                       "entry.786666914" : lname,
+                       "entry.113893232" : address,
+                       "entry.475910473" : city,
+                       "entry.1430871484": province,
+                       "entry.2000257846": postalcode},
                 type: "POST",
                 dataType: "xml",
                 statusCode: {
@@ -81,6 +75,29 @@ $(document).ready(function() {
                       $("input.darkgrey.form-control.city").val("");
                       $("input.darkgrey.form-control.province").val("");
                       $("input.darkgrey.form-control.postal").val("");
+                    },
+                    //400 - Bad Request
+                    400: function(){
+                      alert("There was error within the server. Please try again at later time.");
+                      //Clearing fields after submission
+                      $("input.darkgrey.form-control.fnm").val("");
+                      $("input.darkgrey.form-control.lnm").val("");
+                      $("input.darkgrey.form-control.address").val("");
+                      $("input.darkgrey.form-control.city").val("");
+                      $("input.darkgrey.form-control.province").val("");
+                      $("input.darkgrey.form-control.postal").val("");
+                    }
+                    ,
+                    //404 - Not Found
+                    404: function(){
+                      alert("There was error with the form. Please try again at later time.");
+                      //Clearing fields after submission
+                      $("input.darkgrey.form-control.fnm").val("");
+                      $("input.darkgrey.form-control.lnm").val("");
+                      $("input.darkgrey.form-control.address").val("");
+                      $("input.darkgrey.form-control.city").val("");
+                      $("input.darkgrey.form-control.province").val("");
+                      $("input.darkgrey.form-control.postal").val("");
                     }
                 }
             });
@@ -88,3 +105,35 @@ $(document).ready(function() {
   })
 
 })
+
+// We are listening. :)
+         ▒▓
+       ▓██░
+     ░██    ░██     ░▓█████▓░
+    ██▓    ██▓   ▒███▒░    ▓███▓
+  ░██░   ██░   ███     ▒▓▓▒   ▓███
+  ██    ██   ▒██   ▒██████████  ███░
+ ██░   ██   ███  ▒███░       ██░  ██▓
+ ██   ██   ███  ███▒   ░░░ ░   █▓  ██▓
+▒█   ░██   ██  ▓██▒ ░░░       █ █▒  ███
+██   ██   ██   ███ ░░░       █▓ ██   ██░
+█   ▒██   ██  ░██  ░░     ███▒   ██   ██
+    ██░   █   ▓█▓ ░░░    ██   ░░ ▓█░
+    ██   ▒█ ░ ██  ░░      ██▓     █▓
+    ██   ██ ░ ██ ░░    ░▒  ▒▒▒█▓ ▒█▒
+    ██   ██   █▓ ░  ▓██████▓▓▓▓░██▒
+     █   ██   █▒   ██░   ▒░░░░▒▓░
+         ██  ▒█   ▒█   ░█▓▒▓█▓
+         ██  ▒█   ██        ▓████▒
+          █   █   ██  ░ ░░    ██ ░░
+          ██  █▓  ██  ░░░░░  █▓ ░░░░
+           █░  █░  ██  ░░    ██░  ░
+            ██  ▓█  ▒█░  ▒█▒  ▒██▓
+             ██▓  ██  ███▓░██   ░█
+              ▓██░ ░██      ████ ▒ ██
+                ███ ░█  ░░░  ██▒   ██░
+                 ██▒   ░░░░░░   ░ ▒██
+                  ██  ░░░░░░░░░░  ██▒
+                   ██   ░░░░░░   ██░
+                     ░██▒       ░▓██
+                     ░███▓▓▓▓▓▒░
