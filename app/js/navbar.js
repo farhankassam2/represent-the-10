210966620ffc2d4e7db38e983005f5fb888d2635
EWaterman <evanwaterman@hotmail.com>
2016-11-16 00:25:07 -0800

    finished navbar excluding mobile support

--- app/js/navbar.js
+++ app/js/navbar.js
@@ -5,18 +5,27 @@
  */
 $(window).scroll(function() {
     if ($(document).scrollTop() > 200) {
-        $('#navbar-extra').hide('slow', function() {
-            // Animation complete.
-        });
+        $(".navbar-brand").removeClass("tilt");
         $('.nav-text').hide('slow', function() {
             // Animation complete.
         });
     } else {
-        $('#navbar-extra').show('slow', function() {
-            // Animation complete.
-        });
+        $(".navbar-brand").addClass("tilt");
         $('.nav-text').show('slow', function() {
             // Animation complete.
         });
     }
+});
+
+/*
+ Applies the "active" class to nav elements that have an href to the current page
+ input: NONE
+ returns: NONE
+ */
+$(document).ready(function() {
+    $("nav [href]").each(function() {
+        if (this.href.split("?")[0] == window.location.href.split("?")[0]) {
+            $(this).addClass("active");
+        }
+    });
 });
